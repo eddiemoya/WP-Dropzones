8acae6a393d52d2b8433f892c1f0433d656cc9b6
Eddie Moya <eddie.moya@gmail.com>
2012-07-24 17:18:15 -0500

    typo again

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@ function add_dropzones($dropzones) {
         'name' => 'Custom Dropzone',
         'id' => 'custom',
         'description' => 'Use this area to control this pages layout',
-        'before_widget' => '<article class="content-container dropzone-widget widget %2$s" id="$1s">',
+        'before_widget' => '<article class="content-container dropzone-widget widget %2$s" id="%1s">',
         'after_widget' => '</article>',
         'before_title' => '<header class="content-header"><h3>',
         'after_title' => '</h3></header>'
