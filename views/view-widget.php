51ad773fb8cb8aacc5fce72ca4192a07fbc563c7
Eddie Moya <eddie.moya@gmail.com>
2012-08-29 09:57:13 -0500

    adding widget view

---
+++ views/view-widget.php
@@ -0,0 +1,15 @@
+<div class="sidebar-name">
+    <div class="sidebar-name-arrow"><br /></div>
+    <h3>
+        <?php echo 'Dropzone: ' . esc_html($dropzone->name); ?>
+        <span>
+            <img src="<?php echo esc_url(admin_url('images/wpspin_dark.gif')); ?>" class="ajax-feedback" title="" alt="" />
+        </span>
+    </h3>
+</div>
+<div id="wpdz-sidebar-layout-manager-337" class="widgets-sortables ui-sortable" style="min-height: 50px; ">
+	<div class="sidebar-description">
+		<p class="description">Use this area to control this pages layout<?php echo "<pre>";print_r($dropzone);echo "</pre>";?></p>
+	</div>
+	
+</div>
