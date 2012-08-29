e3222d95fb175d5da8ac255507e607ba1e4c2bac
Eddie Moya <eddie.moya@gmail.com>
2012-08-29 09:36:39 -0500

    replacing the old calls to built-in wordpress widget/sidebar handlers, with our own new dropzone controller

--- views/wpdz-sidebar.php
+++ views/wpdz-sidebar.php
@@ -1,12 +1,9 @@
 <div class="widgets-holder-wrap <?php echo esc_attr($this->get_wrap_class()); ?>">
-    <div class="sidebar-name">
-        <div class="sidebar-name-arrow"><br /></div>
-        <h3>
-            <?php echo esc_html($this->name); ?>
-            <span>
-                <img src="<?php echo esc_url(admin_url('images/wpspin_dark.gif')); ?>" class="ajax-feedback" title="" alt="" />
-            </span>
-        </h3>
-    </div>
-    <?php wp_list_widget_controls($this->id); // Show the control forms for each of the widgets in this sidebar ?>
+    <?php 
+        $dropzones = new WidgetPress_Model_Dropzone(); 
+        
+        foreach((array)$dropzones->get_dropzones('layout') as $dropzone) {
+            include(WPDZ_VIEWS . 'view-widget.php');
+        }
+    ?>
 </div> 
