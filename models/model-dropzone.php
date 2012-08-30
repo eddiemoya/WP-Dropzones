a20c0399ea79cf6609857e5456b7e5fbe458e15c
Eddie Moya <eddie.moya@gmail.com>
2012-08-30 02:24:03 -0500

    dropzone model now can retrieve its own data when given any small part of it

--- models/model-dropzone.php
+++ models/model-dropzone.php
@@ -20,22 +20,25 @@ class WidgetPress_Model_Dropzone {
 	/**
 	 * 
 	 */
-	public function __construct($post_id = null, $type = 'dropzone'){
-
-	}
+	private $meta;
 
 	/**
 	 * 
 	 */
-	private function post_id($post_id = null){
-		if(is_null($post_id)){
-			global $post_id;
+	public function __construct($term = null, $post = null, $type = 'dropzone'){
 
-			if(empty($post_id)){
-				$post_id = $_REQUEST['post_id'];
-			}
+		$this->type 	= $type;
+		$this->term 	= $term;
+
+		if(is_null($post)){
+			$this->post = $this->create_dropzone($this->term->term_id, $type);
+		} else {
+			$this->post = (is_object($post)) ? $post : get_post($post);
 		}
-		return $post_id;
+
+		if(!empty($this->post)){
+			$this->get_dropzone_meta();
+		}
 	}
 
 	/**
@@ -48,51 +51,27 @@ class WidgetPress_Model_Dropzone {
 	/**
 	 * Setter
 	 */
-	private function set($property, $value){
+	protected function set($property, $value){
 			$this->$property = $value;
 	}
 
 	/**
 	 * 
 	 */
-	public function get_dropzones($type = 'dropzone', $post_id = null){
-		$terms =  get_the_terms( $this->post_id($post_id), $type );
-		$terms = ($type == 'layout') ? array($terms[0]) : $terms;
-		foreach($terms as &$term){
-			$post = get_posts(
-				array(
-					'post_type' => $type,
-					'posts_per_page' => 1,
-					'tax_query' => array(
-						array(
-							'operator' => 'IN',
-							'taxonomy' => $type,
-							'field' => 'id',
-							'terms' => $term->term_id
-						)
-					)
-				)
-			);
-			$term->post = $post;
+	public function create_dropzone($term_id, $type = 'dropzone'){
+		if(!empty($term_id)){
+			$this->post = $this->insert_post($term_id, $type);
+			$this->meta = $this->set_dropzone_meta();
 		}
 
-		return $terms;
+		return $this;
 	}
 
 	/**
 	 * 
 	 */
-	public function create_dropzone($term_id, $tax_id = null, $type = 'dropzone'){
-		$this->type = $type;
-		$this->term = get_term($term_id, $type);
-		$this->post = $this->insert_post($term_id, $tax_id, $type);
-	}
-
-	/**
-	 * 
-	 */
-	private function insert_post($term_id, $tax_id = null){
-		$tax = get_taxonomy($this->type);
+	private function insert_post($term_id, $type){
+		$tax = get_taxonomy($type);
 
 		$post_id = wp_insert_post(array(
 			'post_status' 	=> 'publish',
@@ -104,6 +83,20 @@ class WidgetPress_Model_Dropzone {
 
 		return get_post($post_id);
 
+	}
+
+	public function view($template, $args = array()){
+		extract($args);
+		include (WPDZ_VIEWS . $template);
+	}
+
+	/**
+	 * 
+	 */
+	private function get_dropzone_meta(){
+		$meta = get_post_custom($this->post->ID);
+		unset($meta['_edit_lock']);
+		$this->meta = $meta;
 	}
 
 
