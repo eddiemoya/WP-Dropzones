8285203c70492f6cadf8694a94425dd7e716aeea
Eddie Moya <eddie.moya@gmail.com>
2012-08-30 02:24:25 -0500

    not yet fully functional but its getting there

--- views/view-widget.php
+++ views/view-widget.php
@@ -1,15 +1,61 @@
-<div class="sidebar-name">
-    <div class="sidebar-name-arrow"><br /></div>
-    <h3>
-        <?php echo 'Dropzone: ' . esc_html($dropzone->name); ?>
-        <span>
-            <img src="<?php echo esc_url(admin_url('images/wpspin_dark.gif')); ?>" class="ajax-feedback" title="" alt="" />
-        </span>
-    </h3>
-</div>
-<div id="wpdz-sidebar-layout-manager-337" class="widgets-sortables ui-sortable" style="min-height: 50px; ">
-	<div class="sidebar-description">
-		<p class="description">Use this area to control this pages layout<?php echo "<pre>";print_r($dropzone);echo "</pre>";?></p>
+<?php 
+
+
+$options = (object)$widget->widget_options; 
+$widget_class = get_class($widget); 
+
+$widget = new $widget_class();
+echo "<pre>";print_r($widget);echo "</pre>"; 
+
+if(is_subclass_of($widget_class, 'WP_Widget')){ 
+	?>
+
+	<div id="" class="widget ui-draggable"> 
+	    <div class="widget-top">
+	        <div class="widget-title-action">
+	            <a class="widget-action hide-if-no-js" href="#available-widgets"></a>
+	            <a class="widget-control-edit hide-if-js" href="/wp-admin/widgets.php?editwidget_class=<?php echo $widget_class; ?>">
+	                <span class="edit">Edit</span>
+	                <span class="add">Add</span></a>
+	        </div>
+	        <div class="widget-title">
+	            <h4>
+	                <?php echo $widget->name; ?>
+	                <span class="in-widget-title"></span>
+	            </h4>
+	        </div>
+	    </div>
+
+	    <div class="widget-inside">
+	        <form action="" method="post">
+	            <div class="widget-content">
+
+	                <?php $widget->form(array()); ?>
+	            </div>
+	            
+	            <input type="hidden" name="id_base" class="id_base" value="archives">
+	            <input type="hidden" name="widget-width" class="widget-width" value="250">
+	            <input type="hidden" name="widget-height" class="widget-height" value="200">
+	            <input type="hidden" name="add_new" class="add_new" value="multi">
+	            <input type="hidden" name="classname" class="classname" value="<?php echo $options->classname; ?>">
+	            <input type="hidden" name="widget-class" class="widget-class" value="<?php echo $widget_class; ?>">
+
+	            <div class="widget-control-actions">
+	                <div class="alignleft">
+	                    <a class="widget-control-remove" href="#remove">Delete</a> |
+	                    <a class="widget-control-close" href="#close">Close</a>
+	                </div>
+	                <div class="alignright">
+	                    <img src="http://com.loc/wp-admin/images/wpspin_light.gif" class="ajax-feedback" title="" alt="" style="visibility: hidden; ">
+	                    <input type="submit" name="savewidget" id="widget-archives-__i__-savewidget" class="button-primary widget-control-save" value="Save">
+	                </div>
+	                <br class="clear">
+	            </div>
+	        </form>
+	    </div>
+
+	    <div class="widget-description">
+	        <?php echo $options->description; ?>
+	    </div>
 	</div>
-	
-</div>
+<?php }
