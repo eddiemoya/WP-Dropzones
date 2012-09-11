560675495867e37be109e4d4080184bd0bc13bb1
Eddie Moya <eddie.moya@gmail.com>
2012-09-11 00:36:12 -0500

    changing the post type metabox to allow for post type arhchives

--- models/model-metabox-posttypes.php
+++ models/model-metabox-posttypes.php
@@ -51,14 +51,24 @@ class WPDZ_Metabox_Post_Types extends WPDZ_Metabox {
             'label' => 'Category Archive',
             'label_help' => 'Category Archives with no post type filter: "/category/{term}"'
         ),
+         'widgetpress_post_archive' => array(
+            'label' => 'Posts Archive',
+        ),
+        'widgetpress_question_archive' => array(
+            'label' => 'Questions Archive',
+        ),
+        'widgetpress_guide_archive' => array(
+            'label' => 'Guides Archive',
+            'label_help' => 'Select a post type to have this show up on: "category/{post_type}"'
+        ),
         'widgetpress_post_type_post' => array(
-            'label' => 'Posts',
+            'label' => 'Categorized Posts',
         ),
         'widgetpress_post_type_question' => array(
-            'label' => 'Questions',
+            'label' => 'Categorized Questions',
         ),
         'widgetpress_post_type_guides' => array(
-            'label' => 'Guides',
+            'label' => 'Categorized Guides',
             'label_help' => 'Select a post type to have this show up on: "category/{term}/{post_type}"'
         ),
     );
