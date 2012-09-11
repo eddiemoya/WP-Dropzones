7655abc1781dd53a7cf44e8f315e8ebd27482ce9
Eddie Moya <eddie.moya@gmail.com>
2012-09-10 21:48:12 -0500

    thumbnails shit was spelled wrong in the post type delaration

--- controllers/controller-widgets.php
+++ controllers/controller-widgets.php
@@ -225,7 +225,7 @@ class WidgetPress_Controller_Widgets {
 			'has_archive' => false, 
 			'hierarchical' => false,
 			'menu_position' => null,
-			'supports' => array( 'title', 'thumbnails', 'custom-fields' )
+			'supports' => array( 'title', 'thumbnail', 'custom-fields' )
 		); 
 
 		register_post_type('widget', $args);
