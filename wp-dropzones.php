6e50820445f6f0077ffd3fefbc45aaaa628ac99a
Eddie Moya <eddie.moya@gmail.com>
2013-02-05 20:09:45 -0600

    versioning up

--- wp-dropzones.php
+++ wp-dropzones.php
@@ -3,7 +3,7 @@
 Plugin Name: WP Dropzones
 Plugin URI: http://wordpress.org/extend/plugins/wp-dropzones
 Description: Magical things that sound like fun, but might end badly.
-Version: 0.7 Alpha 1
+Version: 0.7 beta 4
 Author: Eddie Moya
 Author URI: http://eddiemoya.com
 */
