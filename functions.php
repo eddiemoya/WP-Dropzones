0139e02352d8cb133406430836da90f8d05d35c6
Eddie Moya <eddie.moya@gmail.com>
2013-02-06 19:58:29 -0600

    making the category awareness of single dropzones cascade down to subcategories

--- functions.php
+++ functions.php
@@ -41,17 +41,33 @@ function display_dropzone_by_terms($slug = null, $categories){
  * This is bad, please dont use this for anything else - it WILL be removed, and you will be shamed!
  * Do not look at this as an example of how to do anything - this is bad, and you are bad for even looking at it. Bad developer! Bad! Shoo, go away now.
  *
+ * Added for single pages to use.
+ *
  * @author Eddie Moya
  */
 function get_first_available_slug_from_list_of_terms_dont_use_this_horrible_function($slug, $categories, $bool = false){
     global $wpdb;
 
+    $slugs = array();
+    $parents = array();
+
+    //Go through each category
     foreach($categories as $category){
+
         $slugs[] = $slug . '_' . $category->slug;
+
+        if($category->parent != 0){
+            //This is the worst thing ever. Really, its like the hilter of the communities sites.
+            $parent_slugs[] = $slug . '_' . get_term_by('id', $category->parent, 'category' )->slug;
+        }
+
     }
 
-    $slugs[] = $slug;
+   
+    $slugs = array_merge($slugs, $parent_slugs);
+     $slugs[] = $slug;
 
+    print_pre($slugs);
     //Stolen from the wp_unique_post_slug() function in wp-includes/post.php
     //Couldnt use that function because it requires an actual post object id to be passed, which we dont have or may not even exist in this case
     $check_sql = "SELECT post_name FROM $wpdb->posts WHERE post_name IN ( '".implode( "', '", esc_sql($slugs))."' ) AND post_type = %s ORDER BY field(post_name, '".implode( "', '", esc_sql($slugs))."' ) LIMIT 1";
